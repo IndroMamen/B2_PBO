32d15676b48cb11e0860089f603c7408723f895a
IndroMamen <indro.man74@gmail.com>
2022-04-27 01:48:55 +0800

    POSTTEST5

---
+++ 2009106099_Indro Dwi Saputro/src/posttest5/Asin.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package posttest5;
+
+import posttest4.*;
+
+/**
+ *
+ * @author ACER
+ */
+final class Asin extends Martabak{
+  private final String nama;
+  private String rasa;
+  private String ukuran;
+  
+  public Asin(String nama, int harga, String rasa, String ukuran){
+      super(nama, harga);
+      this.nama = nama;
+      this.rasa = rasa;
+      this.ukuran = ukuran;
+  }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+    
+    @Override
+  public void display(){
+      System.out.println("Nama           : " + this.nama);
+      System.out.println("Harga          : " + this.harga);
+      System.out.println("Rasa           : " + this.rasa);
+      System.out.println("Ukuran         : " + this.ukuran);
+      pesan();
+}
+  
+  protected void pesan(){
+      System.out.println("Martabak sudah di pesan");
+  }
+  
+    public void pembeli(){
+      System.out.println("Martabak Dibeli oleh orang");
+  }
+  public void pembeli(String orang){
+      System.out.println("Martabak Dibeli oleh   : "+orang);
+  }
+}
+
+
